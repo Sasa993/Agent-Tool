7280b085d9c77d5bd95f1d2dd98340ef8f878b25
Sasa993 <jovicic.sasa@hotmail.com>
2018-04-27 18:45:53 +0200

    testing something out

--- testic.py
+++ testic.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt4 import QtCore, QtGui, uic
 
-qtCreatorFile = "guitest.ui"
+qtCreatorFile = "gui_fajl.ui"
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
@@ -10,6 +10,14 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 		QtGui.QMainWindow.__init__(self)
 		Ui_MainWindow.__init__(self)
 		self.setupUi(self)
+		self.button.clicked.connect(self.testirana_funkcija)
+
+	def testirana_funkcija(self):
+		kita = str(self.prvi_input.text())
+		kita2 = str(self.drugi_input.text())
+
+		# print("Prvo je {} a drugo je {}".format(kita, kita2))
+		self.output_label.setText(kita + " " + kita2)
 
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
