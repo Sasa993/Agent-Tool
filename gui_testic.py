21ae548aeb2e621be41c486d072b87bb9b8a798d
Sasa993 <jovicic.sasa@hotmail.com>
2018-06-03 00:36:54 +0200

    finished adding counters to the selection widget (GUI)... fixed the bug with items text align in QTableWidget...Fixed the bug with selection > details (data was wrong)... ticket number is now clickable/markable and title for every ticket has a description of that ticket

--- gui_testic.py
+++ gui_testic.py
@@ -26,21 +26,21 @@ class Ui_testWidget(object):
     def setupUi(self, testWidget):
         testWidget.setObjectName(_fromUtf8("testWidget"))
         testWidget.setWindowModality(QtCore.Qt.NonModal)
-        testWidget.resize(726, 594)
-        testWidget.setMinimumSize(QtCore.QSize(0, 0))
-        testWidget.setMaximumSize(QtCore.QSize(800, 16777215))
-        self.widget = QtGui.QWidget(testWidget)
-        self.widget.setGeometry(QtCore.QRect(160, 70, 551, 501))
-        self.widget.setObjectName(_fromUtf8("widget"))
-        self.gridLayout_2 = QtGui.QGridLayout(self.widget)
-        self.gridLayout_2.setContentsMargins(10, -1, 10, -1)
+        testWidget.resize(753, 652)
+        testWidget.setMinimumSize(QtCore.QSize(753, 652))
+        testWidget.setMaximumSize(QtCore.QSize(753, 652))
+        self.layoutWidget = QtGui.QWidget(testWidget)
+        self.layoutWidget.setGeometry(QtCore.QRect(197, 70, 551, 501))
+        self.layoutWidget.setObjectName(_fromUtf8("layoutWidget"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.layoutWidget)
+        self.gridLayout_2.setContentsMargins(5, -1, 5, -1)
         self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
-        self.scrollArea = QtGui.QScrollArea(self.widget)
+        self.scrollArea = QtGui.QScrollArea(self.layoutWidget)
         self.scrollArea.setFrameShape(QtGui.QFrame.NoFrame)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName(_fromUtf8("scrollArea"))
         self.scrollAreaWidgetContents = QtGui.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 531, 499))
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 541, 499))
         self.scrollAreaWidgetContents.setObjectName(_fromUtf8("scrollAreaWidgetContents"))
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.scrollAreaWidgetContents)
         self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
@@ -96,16 +96,10 @@ class Ui_testWidget(object):
         self.tableWidgetIspisIzBaze.verticalHeader().setDefaultSectionSize(30)
         self.tableWidgetIspisIzBaze.verticalHeader().setHighlightSections(False)
         self.verticalLayout_2.addWidget(self.tableWidgetIspisIzBaze)
-        self.pushButtonSelektovano = QtGui.QPushButton(self.scrollAreaWidgetContents)
-        self.pushButtonSelektovano.setObjectName(_fromUtf8("pushButtonSelektovano"))
-        self.verticalLayout_2.addWidget(self.pushButtonSelektovano)
-        self.pushButtonRefresh = QtGui.QPushButton(self.scrollAreaWidgetContents)
-        self.pushButtonRefresh.setObjectName(_fromUtf8("pushButtonRefresh"))
-        self.verticalLayout_2.addWidget(self.pushButtonRefresh)
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.gridLayout_2.addWidget(self.scrollArea, 0, 0, 1, 1)
         self.horizontalLayoutWidget = QtGui.QWidget(testWidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(260, 30, 331, 31))
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(400, 9, 331, 51))
         self.horizontalLayoutWidget.setObjectName(_fromUtf8("horizontalLayoutWidget"))
         self.horizontalLayout_5 = QtGui.QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout_5.setObjectName(_fromUtf8("horizontalLayout_5"))
@@ -115,29 +109,158 @@ class Ui_testWidget(object):
         self.pushButton = QtGui.QPushButton(self.horizontalLayoutWidget)
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.horizontalLayout_5.addWidget(self.pushButton)
-        self.gridLayoutWidget = QtGui.QWidget(testWidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 69, 143, 301))
-        self.gridLayoutWidget.setObjectName(_fromUtf8("gridLayoutWidget"))
-        self.gridLayout_7 = QtGui.QGridLayout(self.gridLayoutWidget)
-        self.gridLayout_7.setObjectName(_fromUtf8("gridLayout_7"))
-        self.labelBrojTiketaMjesec = QtGui.QLabel(self.gridLayoutWidget)
-        self.labelBrojTiketaMjesec.setObjectName(_fromUtf8("labelBrojTiketaMjesec"))
-        self.gridLayout_7.addWidget(self.labelBrojTiketaMjesec, 1, 1, 1, 1)
-        self.label_3 = QtGui.QLabel(self.gridLayoutWidget)
-        self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.gridLayout_7.addWidget(self.label_3, 1, 0, 1, 1)
-        self.labelBrojTiketaDanas = QtGui.QLabel(self.gridLayoutWidget)
+        self.verticalLayoutWidget = QtGui.QWidget(testWidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(26, 70, 171, 490))
+        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
+        self.verticalLayout_3 = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout_3.setContentsMargins(-1, 60, -1, 60)
+        self.verticalLayout_3.setSpacing(0)
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.labelkita = QtGui.QLabel(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.labelkita.setFont(font)
+        self.labelkita.setStyleSheet(_fromUtf8("QLabel { \n"
+"border-left: 1px solid black;\n"
+"border-right: 1px solid black;\n"
+"border-top: 1px solid black;\n"
+"background-color: rgb(84, 255, 155);\n"
+"}"))
+        self.labelkita.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelkita.setObjectName(_fromUtf8("labelkita"))
+        self.verticalLayout_3.addWidget(self.labelkita)
+        self.labelBrojTiketaDanas = QtGui.QLabel(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.labelBrojTiketaDanas.setFont(font)
+        self.labelBrojTiketaDanas.setStyleSheet(_fromUtf8("QLabel {\n"
+"background-color: rgb(196, 255, 218);\n"
+"border-top: 1px solid black;\n"
+"}\n"
+"QLabel:hover {\n"
+"border-top: 0;\n"
+"border-right: 1px solid black;\n"
+"border-left: 1px solid black;\n"
+"border-bottom: 1px solid black\n"
+"}"))
+        self.labelBrojTiketaDanas.setAlignment(QtCore.Qt.AlignCenter)
         self.labelBrojTiketaDanas.setObjectName(_fromUtf8("labelBrojTiketaDanas"))
-        self.gridLayout_7.addWidget(self.labelBrojTiketaDanas, 0, 1, 1, 1)
-        self.label = QtGui.QLabel(self.gridLayoutWidget)
-        self.label.setObjectName(_fromUtf8("label"))
-        self.gridLayout_7.addWidget(self.label, 0, 0, 1, 1)
-        self.label_2 = QtGui.QLabel(self.gridLayoutWidget)
+        self.verticalLayout_3.addWidget(self.labelBrojTiketaDanas)
+        self.label_3 = QtGui.QLabel(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setStyleSheet(_fromUtf8("QLabel { \n"
+"border-left: 1px solid black;\n"
+"border-right: 1px solid black;\n"
+"border-top: 1px solid black;\n"
+"background-color: rgb(84, 255, 155);\n"
+"}"))
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.verticalLayout_3.addWidget(self.label_3)
+        self.labelBrojTiketaMjesec = QtGui.QLabel(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.labelBrojTiketaMjesec.setFont(font)
+        self.labelBrojTiketaMjesec.setStyleSheet(_fromUtf8("QLabel {\n"
+"background-color: rgb(196, 255, 218);\n"
+"border-top: 1px solid black;\n"
+"}\n"
+"QLabel:hover {\n"
+"border-top: 0;\n"
+"border-right: 1px solid black;\n"
+"border-left: 1px solid black;\n"
+"border-bottom: 1px solid black\n"
+"}"))
+        self.labelBrojTiketaMjesec.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelBrojTiketaMjesec.setObjectName(_fromUtf8("labelBrojTiketaMjesec"))
+        self.verticalLayout_3.addWidget(self.labelBrojTiketaMjesec)
+        self.label_2 = QtGui.QLabel(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_2.setFont(font)
+        self.label_2.setStyleSheet(_fromUtf8("QLabel { \n"
+"border-left: 1px solid black;\n"
+"border-right: 1px solid black;\n"
+"border-top: 1px solid black;\n"
+"background-color: rgb(84, 255, 155);\n"
+"}"))
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.gridLayout_7.addWidget(self.label_2, 2, 0, 1, 1)
-        self.labelSrednjaVrijednostDuzine = QtGui.QLabel(self.gridLayoutWidget)
+        self.verticalLayout_3.addWidget(self.label_2)
+        self.labelSrednjaVrijednostDuzine = QtGui.QLabel(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.labelSrednjaVrijednostDuzine.setFont(font)
+        self.labelSrednjaVrijednostDuzine.setStyleSheet(_fromUtf8("QLabel {\n"
+"background-color: rgb(196, 255, 218);\n"
+"border-top: 1px solid black;\n"
+"}\n"
+"QLabel:hover {\n"
+"border-top: 0;\n"
+"border-right: 1px solid black;\n"
+"border-left: 1px solid black;\n"
+"border-bottom: 1px solid black\n"
+"}"))
+        self.labelSrednjaVrijednostDuzine.setAlignment(QtCore.Qt.AlignCenter)
         self.labelSrednjaVrijednostDuzine.setObjectName(_fromUtf8("labelSrednjaVrijednostDuzine"))
-        self.gridLayout_7.addWidget(self.labelSrednjaVrijednostDuzine, 2, 1, 1, 1)
+        self.verticalLayout_3.addWidget(self.labelSrednjaVrijednostDuzine)
+        self.horizontalLayoutWidget_2 = QtGui.QWidget(testWidget)
+        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(216, 580, 511, 61))
+        self.horizontalLayoutWidget_2.setObjectName(_fromUtf8("horizontalLayoutWidget_2"))
+        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.horizontalLayout_2.setContentsMargins(-1, 0, -1, 10)
+        self.horizontalLayout_2.setSpacing(50)
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.pushButtonSelektovano = QtGui.QPushButton(self.horizontalLayoutWidget_2)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButtonSelektovano.sizePolicy().hasHeightForWidth())
+        self.pushButtonSelektovano.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButtonSelektovano.setFont(font)
+        self.pushButtonSelektovano.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.pushButtonSelektovano.setStyleSheet(_fromUtf8("QPushButton:hover {background-color: qlineargradient(spread:pad, x1:1, y1:0, x2:0.988636, y2:0.983, stop:0 rgba(96, 255, 170, 255), stop:0.664773 rgba(255, 255, 255, 255));}\n"
+"QPushButton:pressed { border: 1px solid black;}\n"
+"QPushButton { background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:1, y2:0, stop:0 rgba(96, 255, 170, 255), stop:0.664773 rgba(255, 255, 255, 255)); border-radius: 20px; }\n"
+"\n"
+""))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("images/005-select.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButtonSelektovano.setIcon(icon)
+        self.pushButtonSelektovano.setIconSize(QtCore.QSize(32, 32))
+        self.pushButtonSelektovano.setObjectName(_fromUtf8("pushButtonSelektovano"))
+        self.horizontalLayout_2.addWidget(self.pushButtonSelektovano)
+        self.pushButtonRefresh = QtGui.QPushButton(self.horizontalLayoutWidget_2)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButtonRefresh.sizePolicy().hasHeightForWidth())
+        self.pushButtonRefresh.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButtonRefresh.setFont(font)
+        self.pushButtonRefresh.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.pushButtonRefresh.setStyleSheet(_fromUtf8("QPushButton:hover {background-color: qlineargradient(spread:pad, x1:1, y1:0, x2:0.988636, y2:0.983, stop:0 rgba(96, 255, 170, 255), stop:0.664773 rgba(255, 255, 255, 255));}\n"
+"QPushButton:pressed { border: 1px solid black;}\n"
+"QPushButton { background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:1, y2:0, stop:0 rgba(96, 255, 170, 255), stop:0.664773 rgba(255, 255, 255, 255)); border-radius: 20px; }\n"
+"\n"
+""))
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(_fromUtf8("images/001-circular-arrow.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButtonRefresh.setIcon(icon1)
+        self.pushButtonRefresh.setIconSize(QtCore.QSize(32, 32))
+        self.pushButtonRefresh.setObjectName(_fromUtf8("pushButtonRefresh"))
+        self.horizontalLayout_2.addWidget(self.pushButtonRefresh)
 
         self.retranslateUi(testWidget)
         QtCore.QMetaObject.connectSlotsByName(testWidget)
@@ -155,15 +278,15 @@ class Ui_testWidget(object):
         item.setText(_translate("testWidget", "Status", None))
         item = self.tableWidgetIspisIzBaze.horizontalHeaderItem(4)
         item.setText(_translate("testWidget", "Date", None))
-        self.pushButtonSelektovano.setText(_translate("testWidget", "SELECT", None))
-        self.pushButtonRefresh.setText(_translate("testWidget", "REFRESH", None))
         self.pushButton.setText(_translate("testWidget", "Search", None))
-        self.labelBrojTiketaMjesec.setText(_translate("testWidget", "TextLabel", None))
-        self.label_3.setText(_translate("testWidget", "Mjesec:", None))
-        self.labelBrojTiketaDanas.setText(_translate("testWidget", "TextLabel", None))
-        self.label.setText(_translate("testWidget", "Danas:", None))
-        self.label_2.setText(_translate("testWidget", "Srednja vrijednost", None))
-        self.labelSrednjaVrijednostDuzine.setText(_translate("testWidget", "TextLabel", None))
+        self.labelkita.setText(_translate("testWidget", "Today", None))
+        self.labelBrojTiketaDanas.setText(_translate("testWidget", "a", None))
+        self.label_3.setText(_translate("testWidget", "This month", None))
+        self.labelBrojTiketaMjesec.setText(_translate("testWidget", "a", None))
+        self.label_2.setText(_translate("testWidget", "Avarage handling time", None))
+        self.labelSrednjaVrijednostDuzine.setText(_translate("testWidget", "a", None))
+        self.pushButtonSelektovano.setText(_translate("testWidget", " SELECT", None))
+        self.pushButtonRefresh.setText(_translate("testWidget", " REFRESH", None))
 
 
 if __name__ == "__main__":
--- testic.py
+++ testic.py
@@ -72,27 +72,37 @@ class Ui_testWidget(QtGui.QWidget, Ui_testWidget):
 			# da bi table funkcionisao, moramo dodavati row za svaki
 			rowPosition = self.tableWidgetIspisIzBaze.rowCount()
 			self.tableWidgetIspisIzBaze.insertRow(rowPosition)
-			# da bi ID bio INTEGER da se moze sortirati normalno
+			
 			item = QtGui.QTableWidgetItem()
+			# da bi ID bio INTEGER da se moze sortirati normalno
 			item.setData(QtCore.Qt.EditRole, x[0])
+			item.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+			item1 = QtGui.QTableWidgetItem("{0}".format(x[1]))
+			item1.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+			item2 = QtGui.QTableWidgetItem("{0}".format(x[2]))
+			item2.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+			item3 = QtGui.QTableWidgetItem("{0}".format(x[3]))
+			item3.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+			item4 = QtGui.QTableWidgetItem("{0}".format(x[4]))
+			item4.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
 
 			self.tableWidgetIspisIzBaze.setItem(rowPosition, 0, item)
-			self.tableWidgetIspisIzBaze.setItem(rowPosition, 1, QtGui.QTableWidgetItem("{0}".format(x[1])))
-			self.tableWidgetIspisIzBaze.setItem(rowPosition, 2, QtGui.QTableWidgetItem("{0}".format(x[2])))
-			self.tableWidgetIspisIzBaze.setItem(rowPosition, 3, QtGui.QTableWidgetItem("{0}".format(x[3])))
-			self.tableWidgetIspisIzBaze.setItem(rowPosition, 4, QtGui.QTableWidgetItem("{0}".format(x[4])))
+			self.tableWidgetIspisIzBaze.setItem(rowPosition, 1, item1)
+			self.tableWidgetIspisIzBaze.setItem(rowPosition, 2, item2)
+			self.tableWidgetIspisIzBaze.setItem(rowPosition, 3, item3)
+			self.tableWidgetIspisIzBaze.setItem(rowPosition, 4, item4)
 
 		datumSada = str(datetime.date.today().strftime("%m/%d/%Y"))
 
 		# kveri za ispis broja tiketa za danas
 		c.execute("SELECT COUNT(*) FROM ticket_info WHERE datum = '{0}'".format(str(datetime.date.today().strftime("%m/%d/%Y"))))
 		conn.commit()
-		self.labelBrojTiketaDanas.setText(str(c.fetchone()[0]))
+		self.labelBrojTiketaDanas.setText("{0} ticket(s)".format(str(c.fetchone()[0])))
 
 		# kveri za ispis broja tiketa za trenutni mjesec
 		c.execute("SELECT COUNT(*) FROM ticket_info WHERE month = '{0}'".format(datetime.datetime.strptime(datumSada, "%m/%d/%Y").month))
 		conn.commit()
-		self.labelBrojTiketaMjesec.setText(str(c.fetchone()[0]))
+		self.labelBrojTiketaMjesec.setText("{0} ticket(s)".format(str(c.fetchone()[0])))
 
 		# kveriji za srednju vrijednost trajanja svih poziva
 		brojTiketa = c.execute("SELECT COUNT(*) FROM ticket_info")
@@ -135,31 +145,12 @@ class Ui_selektovaniId(QtGui.QWidget, Ui_selektovaniId):
 		neki_kveri = c.execute("SELECT * FROM ticket_info WHERE id_ticket_info = '{0}'".format(lista[0]))
 
 		for x in neki_kveri:
-			self.plainTextEditOutput.setPlainText("{0}\nSite/Tree/Key #: {1}\nDate / Time issue occurs: {2}\n\nPoint of Contact (First and Last name): {3}\nSite/Point of Contact Phone#: {4}\nSite/Point of Contact Email: {5}\n\nDescription of the Problem:\n{6}\n\nHas site ever called support for the same issue?: {7}\n\nDid it ever work?: {8}\n\nWhen did it stop working: {9}\nChanges made around that time: {10}\n\nHow many terminals on location: {11}\nHow many terminals are down: {12}\nAre any of the affected terminals specialty terminals?: {13}\n\nReproduction and Troubleshooting steps taken to resolve:\n\n{14}\n\nScreen shots attached (if applicable): {15}\nModel & S/N (if hardware related): {16}\nAlternative method that will be used by the site: {17}\n\n***Next Steps for next contact:\n{18}".format(x[10], x[5], x[4], x[6], x[7], x[9], x[22], x[11], x[12], x[13], x[14], x[15], x[16], x[17], x[23], x[18], x[19], x[20], x[21]))
+			self.plainTextEditOutput.setPlainText("{0}\nSite/Tree/Key #: {1}\nDate / Time issue occurs: {2}\n\nPoint of Contact (First and Last name): {3}\nSite/Point of Contact Phone#: {4}\nSite/Point of Contact Email: {5}\n\nDescription of the Problem:\n{6}\n\nHas site ever called support for the same issue?: {7}\n\nDid it ever work?: {8}\n\nWhen did it stop working: {9}\nChanges made around that time: {10}\n\nHow many terminals on location: {11}\nHow many terminals are down: {12}\nAre any of the affected terminals specialty terminals?: {13}\n\nReproduction and Troubleshooting steps taken to resolve:\n\n{14}\n\nScreen shots attached (if applicable): {15}\nModel & S/N (if hardware related): {16}\nAlternative method that will be used by the site: {17}\n\n***Next Steps for next contact:\n{18}".format(x[13], x[8], x[4], x[9], x[10], x[12], x[25], x[14], x[15], x[16], x[17], x[18], x[19], x[20], x[26], x[21], x[22], x[23], x[24]))
 			self.labelIncidentNumber.setText(str(x[1]))
-			self.labelVrijeme.setText(str("{0}h {1}min {2}sec".format(x[24], x[25], x[26])))
-		# 	self.labelDatum.setText(x[4])	----------------
+			self.labelVrijeme.setText(str("{0}h {1}min {2}sec".format(x[27], x[28], x[29])))
 			self.labelSeverity.setText(x[2])
-			# self.labelZipCode.setText(x[8])
-		# 	self.labelVersions.setText(x[10])	-----
-		# 	self.labelSiteKey.setText(x[5])	------
-		# 	self.labelNameLastName.setText(x[6])	----
-		# 	self.labelContactPhone.setText(x[7])	----
-		# 	self.labelContactEmail.setText(x[9])	----
-		# 	self.labelDescription.setText(x[22])	----
-		# 	self.labelTroubleshooting.setText(x[23]) 	-----
-		# 	self.labelNextSteps.setText(x[21])			-----
-		# 	self.labelHasSiteEverCalled.setText(x[11])	----
-		# 	self.labelDidItEverWork.setText(x[12])	------
-		# 	self.labelWhenDidItStop.setText(x[13])	-----
-		# 	self.labelChangesMade.setText(x[14])	----
-		# 	self.labelHowManyTermLoc.setText(x[15])	------
-		# 	self.labelHowManyTermDown.setText(x[16])	----
-		# 	self.labelAreAnySpecTerm.setText(x[17])	------
-		# 	self.labelScreenShotsAttached.setText(x[18]) 	----
-		# 	self.labelModelSerialNum.setText(x[19]) 	-----
-		# 	self.labelAlternativeMethod.setText(x[20]) 	-----
 			self.labelStatus.setText(x[3])
+			self.setWindowTitle(x[25])
 
 		conn.commit()
 		conn.close()
